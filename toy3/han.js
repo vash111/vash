370fc0cd4bb5acbab5460da87f4c2a5908d905fc
vash111 <lese9824@naver.com>
2024-09-25 17:51:50 +0900

    ddd

---
+++ toy3/han.js
@@ -0,0 +1,8 @@
+setInterval(function(){
+    $('#slide2>ul').delay(2500);
+    $('#slide2>ul').animate({marginTop: "-300px"})
+    $('#slide2>ul').delay(2500);
+    $('#slide2>ul').animate({marginTop: "-600px"})
+    $('#slide2>ul').delay(2500);
+    $('#slide2>ul').animate({marginTop: "0px"})
+});
